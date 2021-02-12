dc25bcae73efcb5e9edc05896d9b3b2038f96dbb
Kayla Reopelle <kreopelle@gmail.com>
2021-02-12 07:34:31 -0800

    Update migration syntax errors

--- db/migrate/20210212062558_create_speakers.rb
+++ db/migrate/20210212062558_create_speakers.rb
@@ -2,7 +2,7 @@ class CreateSpeakers < ActiveRecord::Migration[6.1]
   def change
     create_table :speakers do |t|
       t.string :name
-      t.text, :bio
+      t.text :bio
       t.string :role
 
       t.timestamps
